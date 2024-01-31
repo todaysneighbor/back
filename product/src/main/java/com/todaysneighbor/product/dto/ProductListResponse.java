65ec02f6c68aae49a580f31de05176c38d1724b7
Kimkiheon <cbj2741@naver.com>
2024-02-01 01:33:21 +0900

    feat: Product 기능 관련 DTO 추가

---
+++ product/src/main/java/com/todaysneighbor/product/dto/ProductListResponse.java
@@ -0,0 +1,25 @@
+package com.todaysneighbor.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductListResponse {
+    private long productId;
+    private int productPrice;
+    private String filename;
+    private int viewCount;
+    private int wishCount;
+    private String title;
+    private String area;
+    private LocalDateTime createdAt;
+    private LocalDateTime refreshedAt;
+    private int state = 0;
+}
---
+++ product/src/main/java/com/todaysneighbor/product/dto/ProductSearchResultResponse.java
@@ -0,0 +1,13 @@
+package com.todaysneighbor.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductSearchResultResponse {
+}
