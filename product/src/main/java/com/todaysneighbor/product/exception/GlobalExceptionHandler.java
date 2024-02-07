13fb84f25a7ae2c6f099e3c8f8866a05c08827a9
Kimkiheon <cbj2741@naver.com>
2024-02-07 22:32:31 +0900

    feat: Product exception Handling 유효성 검사 추가

--- product/src/main/java/com/todaysneighbor/product/exception/GlobalExceptionHandler.java
+++ product/src/main/java/com/todaysneighbor/product/exception/GlobalExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.todaysneighbor.product.exception;
 
 import com.todaysneighbor.product.exception.business.BusinessException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -10,20 +10,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final String ERROR = "error : {}";
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
-        logger.error("error : {}", e.getMessage());
+        log.error(ERROR, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(BusinessException.class)
     protected ResponseEntity<ErrorResponse> businessException(BusinessException e) {
         ErrorResponse response = ErrorResponse.of(e.getErrorCode());
-        logger.error("error : {}", e.getMessage());
+        log.error(ERROR, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
@@ -31,6 +32,13 @@ public class GlobalExceptionHandler {
     protected ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
         ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE,
                 e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<ErrorResponse> dataIntegrityViolationException(DataIntegrityViolationException e) {
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+        log.error(ERROR, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
