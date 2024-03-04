f3a62fd7ea3eda546ec686fad8c7172bd88467b2
DoHyun <yup8229@gmail.com>
2024-03-05 05:06:26 +0900

    feat: User entity 생성

---
+++ user/src/main/java/com/todaysneighbor/user/domain/entity/User.java
@@ -0,0 +1,40 @@
+package com.todaysneighbor.user.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="providerId")
+    private long providerId;
+
+    @Column(name = "nickname", nullable = false, length = 10)
+    private String nickname;
+
+    @Column(name = "filename", length = 100)
+    private String filename;
+
+    @Column(name = "rating", nullable = false)
+    private float rating = 0;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
+
+}
